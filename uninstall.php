ab9a53d24b7d47f65fe883c003b71793eeb45763
alex.kovalevv <alex.kovalevv@gmail.com>
2018-10-16 15:09:42 +0300

    Добавлено очистка опций при удалении плагина с поддержкой мультисайтов.

--- uninstall.php
+++ uninstall.php
@@ -8,5 +8,16 @@
 	// remove plugin options
 	global $wpdb;
 
-	$wpdb->query("DELETE FROM {$wpdb->prefix}options WHERE option_name LIKE 'wbcr_htm_%';");
+	if( !function_exists('is_plugin_active_for_network') ) {
+		require_once(ABSPATH . 'wp-admin/includes/plugin.php');
+	}
+
+	if( is_multisite() && is_plugin_active_for_network(plugin_basename(dirname(__FILE__) . '/html-minify.php')) ) {
+		$wpdb->query("DELETE FROM {$wpdb->sitemeta} WHERE meta_key LIKE 'wbcr_htm_%';");
+	} else {
+		$wpdb->query("DELETE FROM {$wpdb->options} WHERE option_name LIKE 'wbcr_htm_%';");
+	}
+
+
+
 
