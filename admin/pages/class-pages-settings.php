0e3423849f1007a66d06044c00b961f6596a4996
alex.kovalevv <alex.kovalevv@gmail.com>
2019-08-16 03:03:53 +0300

    Рефакторинг.

--- admin/pages/class-pages-settings.php
+++ admin/pages/class-pages-settings.php
@@ -1,70 +1,84 @@
 <?php
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
 
 /**
  * The page Settings.
  *
  * @since 1.0.0
  */
-
-// Exit if accessed directly
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
-
 class WHTM_SettingsPage extends Wbcr_FactoryClearfy000_PageBase {
 
 	/**
-	 * The id of the page in the admin menu.
-	 *
-	 * Mainly used to navigate between pages.
-	 *
-	 * @since 1.0.0
-	 * @see   Wbcr_FactoryPages000_AdminPage
+	 * {@inheritDoc}
 	 *
 	 * @var string
 	 */
-	public $id = "html_minify"; // Уникальный идентификатор страницы
-	public $page_menu_dashicon = 'dashicons-testimonial'; // Иконка для закладки страницы, дашикон
-	public $page_parent_page = "performance"; // Уникальный идентификатор родительской страницы
+	public $id = "html_minify";
+
 	/**
-	 * Доступена для мультисайтов
+	 * {@inheritDoc}
+	 *
+	 * @var string
+	 */
+	public $page_menu_dashicon = 'dashicons-testimonial';
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @var string
+	 */
+	public $page_parent_page = "performance";
+
+	/**
+	 * {@inheritDoc}
 	 *
 	 * @var bool
 	 */
 	public $available_for_multisite = true;
 
 	/**
-	 * @param Wbcr_Factory000_Plugin $plugin
+	 * {@inheritDoc}
+	 *
+	 * @since  1.1.0
+	 * @var bool
+	 */
+	public $show_right_sidebar_in_options = true;
+
+	/**
+	 * WHTM_SettingsPage constructor.
+	 *
+	 * @author Alexander Kovalev <alex.kovalevv@gmail.com>
+	 *
+	 * @param \Wbcr_Factory000_Plugin $plugin
 	 */
 	public function __construct( Wbcr_Factory000_Plugin $plugin ) {
 		// Заголовок страницы
 		$this->menu_title = __( 'HTML Minify', 'html-minify' );
 
-		// Если плагин загружен, как самостоятельный, то мы меняем настройки страницы и делаем ее внешней,
-		// а не внутренней страницей родительского плагина. Внешнии страницы добавляются в Wordpress меню "Общие"
-
 		if ( ! defined( 'LOADING_HTML_MINIFY_AS_ADDON' ) ) {
-			// true - внутреняя, false- внешняя страница
-			$this->internal = false;
-			// меню к которому, нужно прикрепить ссылку на страницу
-			$this->menu_target = 'options-general.php';
-			// Если true, добавляет ссылку "Настройки", рядом с действиями активации, деактивации плагина, на странице плагинов.
+			$this->internal                   = false;
+			$this->menu_target                = 'options-general.php';
 			$this->add_link_to_plugin_actions = true;
-
-			$this->page_parent_page = null;
+			$this->page_parent_page           = null;
 		}
 
 		parent::__construct( $plugin );
 	}
 
-	// Метод позволяет менять заголовок меню, в зависимости от сборки плагина.
+	/**
+	 * {@inheritDoc}
+	 *
+	 * @var string
+	 */
 	public function getMenuTitle() {
 		return defined( 'LOADING_HTML_MINIFY_AS_ADDON' ) ? __( 'HTML Minify', 'html-minify' ) : __( 'General', 'html-minify' );
 	}
 
 	/**
-	 * Метод должен передать массив опций для создания формы с полями.
-	 * Созданием страницы и формы занимается фреймворк
+	 * {@inheritDoc}
 	 *
 	 * @since 1.0.0
 	 * @return mixed[]
